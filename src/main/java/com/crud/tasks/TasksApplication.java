34738e0c3f19bf05b0bee61c9aac90651360ee74
izagaluszewska <izabellagaluszewska@wp.pl>
2017-12-10 16:26:35 +0100

    zadanie 19_2

--- src/main/java/com/crud/tasks/TasksApplication.java
+++ src/main/java/com/crud/tasks/TasksApplication.java
@@ -2,6 +2,8 @@ package com.crud.tasks;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
 public class TasksApplication {
@@ -10,8 +12,8 @@ public class TasksApplication {
 		SpringApplication.run(TasksApplication.class, args);
 	}
 
-	//@Override
-	//protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-	//	return application.sources(TasksApplication.class);
-	//}
+	/*@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(TasksApplication.class);
+	}*/
 }
